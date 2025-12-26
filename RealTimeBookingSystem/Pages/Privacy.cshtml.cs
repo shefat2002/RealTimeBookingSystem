95793ae74332f17749d6f8a1fde1b8a59077c8d4
Md Shefat Al Mahmud <shefat2002@gmail.com>
2025-12-26 18:07:19 +0600

    initial commit

---
+++ RealTimeBookingSystem/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealTimeBookingSystem.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
